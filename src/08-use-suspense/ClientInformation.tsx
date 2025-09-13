b9e3c7858f85426d7b5fa2f7fd987a87d264205c
Gibran Flores Mendieta <gibran.floresmendieta@gmail.com>
2025-09-12 19:38:33 -0600

    Ultima acatualizacion

--- src/08-use-suspense/ClientInformation.tsx
+++ src/08-use-suspense/ClientInformation.tsx
@@ -1,12 +1,20 @@
+import { use, type Usable } from "react"
+import { getUserAction, type user } from "./api/get-user-action"
 
+interface Props {
+    getUser: Usable<user>
+}
 
-export const ClientInformation = () => {
+export const ClientInformation = ({getUser}:Props) => {
+
+const getuser = use(getUser)
+
     return (
         <div className="bg-gradient flex flex-col gap-4">ClientInformation
-            <h1 className="text-white text-4xl text-thin">Gibran -123</h1>
+            <h1 className="text-white text-4xl text-thin">{getuser.name} - {getuser.id}</h1>
             
-            <p className="text-white text-4xl">CDMX Mexico</p>
-            <p className="text-white text-4xl">rol ususario</p>
+            <p className="text-white text-4xl">{getuser.location}</p>
+            <p className="text-white text-4xl">{getuser.role}</p>
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,39 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'sonner'
 
 import { InstagromApp } from './07-useOptimistic/InstagromApp'
 // import { MemoApp } from './06-useMemo/memoApp'
 // import { HookApp } from './HookApp'
-// import { TasksApp } from './05-useRef/taskApp'
+import { TasksApp } from './05-useRef/taskApp'
 // import { ScrambleWords } from './05-useRef/gameApp'
 // import { ScrambleWordsCopy } from './05-useRef/gameApp copy'
 // import { TrafficLight } from './useState/TrafficLight'
 
 // import { TrafficLightEffect } from './useEffect/TrafficLightEffect'
 
+// import { ClientInformation } from './08-use-suspense/ClientInformation'
+// import { getUserAction } from './08-use-suspense/api/get-user-action'
 // import { MemoCounter } from './06-useMemo/MemoCounter'
 // import { PokemonPage } from './03-Example/PokemonPage'
 // import { FocusScreen } from './04-useReff/FocusScreen'
 // import { TrafficLightEffectHook } from './02-useEffect/TrafficLightEffectHook'
 import './index.css'
-import { ClientInformation } from './08-use-suspense/ClientInformation'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+
+
     <Toaster/>
-    <ClientInformation/>
+
+    <Suspense fallback={
+      <div className='bg-gradient flex flex-col'>
+        <h1 className='text-4xl text-white'>Cargando...</h1>
+      </div>
+    }>
+      {/* <ClientInformation getUser={getUserAction(1458)}/> */}
+    </Suspense>
     {/* <InstagromApp/> */}
     {/* <MemoCounter/> */}
     {/* <MemoApp/> */}
@@ -31,7 +41,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <PokemonPage/> */}
     {/* <TrafficLightEffectHook/> */}
     {/* <HookApp/> */}
-    {/* <TasksApp/> */}
+    <TasksApp/>
     {/* <ScrambleWordsCopy/> */}
   </StrictMode>,
 )
